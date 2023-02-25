9afcc0afa0487502898c8216c26d0dfe6072ef22
fernandoap46 <fernandoap46@gmail.com>
2023-02-24 23:30:40 -0300

    Inclusao dados clientes

---
+++ src/mySQL/inclusaoClientes.sql
@@ -0,0 +1,49 @@
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero,complemento)
+values
+('Melvina José Pedrosa',76263280271,'melvina87@hotmail.com',57309494,'Arapiraca','AL','Rua Ingá',10,'casa 2');
+
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero,complemento)
+values
+('Osnir da Silva',78078957807,'chaz40@hotmail.com',68960971,'Calçoene','AP','Vila Carnot',14,'casa 4')
+
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero,complemento)
+values
+('Maia de Assis',5215776679,'maia4@hotmail.com',29795971,'Águia Branca','ES','Estrada Águas Claras Km 10',22,'casa 3')
+
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero,complemento)
+values
+('Jayda Runol',31357259018,'jayda_runolfsson@yahoo.com',76952970,'Alto Alegre dos Parecis','RO','Rua Tancredo Neves',3494,'casa 1')
+
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero,complemento)
+values
+('Jassinto de Paula',41446647161,'jass@gmail.com',89636970,'Abdon Batista','SC','Avenida Vinte e Seis de Abril'707,'casa 2')
+
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero)
+values
+('ANA BRENDA M SOUZA',46595734192,'ana.brenda@yahoo.com',89188970,'Agronômica','SC','Rua XV de Novembro',681)
+
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero,complemento)
+values
+('Walter Saraiva Junior',42241727393,'zackery72@yahoo.com',88655970,'Águas Brancas','SC','Rodovia SC-430 km 21',55,'em frente o mercado')
+
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero)
+values
+('Marieta Muller',76220682335,'marietta.mueller@gmail.com',83550970,'Açungui','PR','Avenida Principal',586)
+
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero)
+values
+('Hortensia Benevides',87251425343,'hortense.bins@yahoo.com',84538970,'Angai','PR','Rua Principalá',38)
+
+insert into Cliente
+(nome,cpf,email,cep,cidade,estado,endereco,numero)
+values
+('CASSIA ALVES',1373570016,'cassia.alvez@yahoo.com',85754970,'Centro Novo','PR','Rua Principal',96)
