bc96d8a907c9367c39f2c7e886b3aca88d26b96a
Victor <victorfaccioli@hotmail.com>
2023-02-25 22:34:28 -0300

    Criado dados teste para tabela pedido e pagamento. Criado modelo de instução de utilização. Falta colocar imagens de exemplo

---
+++ Instructions.md
@@ -0,0 +1,37 @@
+Instrução de como ter esse banco de dados na sua maquina funcional.
+
+Requisito ter MYSQL Workbench
+
+Exercutar MYSQL Workbench iniciar uma instancia local.
+
+Colocar senha configurada em sua máquina, caso não tenha senha configurada colocar senha padrap root
+
+Clique em Schemas:
+
+Clique em Create a new SQL tab for executing queries:
+
+Escreva o comando que está na basta src/mySQL o arquivo createTable.sql e execute:
+
+Após esse processo todas as tabela foram criadas, com todos atributos definidos segue imagem de exemplo:
+
+Execute o comando: 
+
+use escola;
+
+Você acessou o banco de dados, agora precisa acrescentar os dados de cada tabela.
+
+Para utilizar o banco de dados podera utilizar os comandos:
+
+INSERT:
+Exemplo para inserir dados:
+
+
+DELETE:
+Exemplo para deletar dados:
+
+UPDATE:
+Exemplo para alterar dados:
+
+SELECT:
+Exemplo para mostar dados:
+obs: Criamos essses dados de forma aleatoria para teste pode utilizar os arquivos com nome de inclusao...
---
+++ inclusoaPagamento.sql
@@ -0,0 +1,44 @@
+-----------------------------------------------------------------------
+--- id_pedido tem que colocar o mesmo que foi criado automaticamente---
+--- na tabela pedido para não ter erro---------------------------------
+-----------------------------------------------------------------------
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('pix','B23850K68O554L6123N5','ok',1)
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('boleto','L345JOP61N23B4123V31','pendente',2)
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('credito','LN1232N21Q1A9879X055','cancelado',3)
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('boleto','9AKB4820FKY48FC76349','ok',4)
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('pix','NH4872LN59273HB4509J','ok',5)
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('credito','U31298JNB4876SMN4132','pendente',6)
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('credito','K21MHOFI9NBI13JMMK20','ok',7)
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('credito','B287164H210976J12876','ok',8)
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('pix','EL2349765JH129LMN4569','cancelado',9)
+
+INSERT INTO pagamento (forma_pagamento,autenticacao,status,id_pedido)
+values
+('pix','I234123MN59876DNJ678','ok',10)
